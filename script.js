d517593f287010136bd40eae3b55f7d02e328031
Semyon Gryadunov <147247967+kommers13@users.noreply.github.com>
2024-05-26 17:53:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.getElementById('theme-toggle').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const currentTheme = localStorage.getItem('theme') || 'theme-light';
+    document.body.classList.add(currentTheme);
+
+    document.getElementById('theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('theme-dark');
+    document.body.classList.toggle('theme-light');
+
+    const newTheme = document.body.classList.contains('theme-dark') ? 'theme-dark' : 'theme-light';
+    localStorage.setItem('theme', newTheme);
 });
+});
+
